d24b9e65f8b3d469ef125f2dcdc39d26a1f7ad3a
syeeon <glsy1150@naver.com>
2023-05-24 21:16:00 +0900

    230524 불필요한 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import 'styles/common/reset.css';
 
-// component import
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 import Join from 'page/join'
--- src/page/main.js
+++ src/page/main.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import 'styles/page/main.css'
 import 'styles/responsive/reMain.css'
